d70e2ccd5765046ac4dc2f5caffe392828a5fb44
Andrei Gorevoi <18.andreigorevoi@gmail.com>
2020-02-23 21:06:10 +0300

    First classes for libary_project

---
+++ src/lib_model/GameStart.java
@@ -0,0 +1,21 @@
+package lib_model;
+
+import java.util.Scanner;
+
+public class GameStart {
+    public static void main(String[] args) {
+        String name;
+        String type;
+        String author;
+
+        Library lib = new Library();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("would you like to take a book? Please tell me: name, type and author");
+        name=sc.nextLine();
+        type=sc.nextLine();
+        author=sc.nextLine();
+
+        lib.getMeBook(name,type,author);
+    }
+}
---
+++ src/lib_model/Library.java
@@ -0,0 +1,7 @@
+package lib_model;
+
+public class Library {
+    public void getMeBook(String name, String type, String author){
+        Book book1 = new Book(name,type,author);
+    }
+}
